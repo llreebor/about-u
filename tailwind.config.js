017ab1551293f087686b9c1b8e02c36eef617f81
llreebor <timoschuk.roman@gmail.com>
2024-11-07 14:48:47 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	darkMode: 'class',
+	theme: {
+		container: {
+			center: true,
+			padding: '16px',
+		},
+		extend: {
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+			},
+			colors: {
+				primary: '#130E1D',
+				secondary: '#F9F9F9',
+			},
+			keyframes: {
+				'fade-in': {
+					from: {
+						opacity: 0,
+					},
+					to: {
+						opacity: 1,
+					},
+				},
+			},
+			animation: {
+				fadeIn: 'fade-in .3s ease',
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
+}
